8fa0f6599c0065f2ffc528053c8ecde3e3743127
ysrao1230 <71590325+ysrao1230@users.noreply.github.com>
2020-12-31 17:45:28 +0530

    new changes

--- src/test/java/com/onpassive/Ostaff/Pages/Department.java
+++ src/test/java/com/onpassive/Ostaff/Pages/Department.java
@@ -1,5 +1,14 @@
 package com.onpassive.Ostaff.Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Department {
 
+	WebDriver driver;
+	
+	public Department(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
 }
