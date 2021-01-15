4c990deb337da5904d220b3e7a8b72fb24b31e02
ysrao1230 <71590325+ysrao1230@users.noreply.github.com>
2021-01-15 15:55:11 +0530

    Get window handles

---
+++ src/main/java/Log4j/childWindow.java
@@ -0,0 +1,64 @@
+package Log4j;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+public class childWindow {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/browser-windows");
+
+		// Open new child window within the main window
+		driver.findElement(By.id("windowButton")).click();
+
+		// Get handles of the windows
+		String mainWindowHandle = driver.getWindowHandle();
+		System.out.println("Main window is: " + mainWindowHandle);
+		Set<String> allWindowHandles = driver.getWindowHandles();
+		Iterator<String> iterator = allWindowHandles.iterator();
+
+		// Here we will check if child window has other child windows and will fetch the
+		// heading of the child window
+		while (iterator.hasNext()) {
+			String ChildWindow = iterator.next();
+			if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
+				driver.switchTo().window(ChildWindow);
+				System.out.println("Child window is: " + ChildWindow);
+				WebElement text = driver.findElement(By.id("sampleHeading"));
+				System.out.println("Heading of child window is " + text.getText());
+				System.out.println("Navigating to google.com");
+				driver.get("https://google.com");
+				Thread.sleep(4000);
+			}
+		}
+		System.out.println("Switching to the main window after all child operation performance");
+		driver.switchTo().window(mainWindowHandle);
+		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
+		driver.quit();
+
+		// Make a collection
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("Mazda");
+
+		// Get the iterator
+		Iterator<String> it = cars.iterator();
+		System.out.println(" ");
+		// Print the first item
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+}
