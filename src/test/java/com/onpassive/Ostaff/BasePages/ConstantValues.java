4576dc8616cbc1b2b06ea34d2fcc40a9b5f4f645
ysrao1230 <71590325+ysrao1230@users.noreply.github.com>
2021-04-22 13:24:55 +0530

    changes

--- src/test/java/com/onpassive/Ostaff/BasePages/ConstantValues.java
+++ src/test/java/com/onpassive/Ostaff/BasePages/ConstantValues.java
@@ -4,7 +4,7 @@ public interface ConstantValues {
 	String CHROME_KEY = "webdriver.chrome.driver";
 	String FIREFOX_KEY = "webdriver.gecko.driver";
 	String EDGE_KEY = "webdriver.edge.driver";
-	String CHROME_PATH = "./driver/chromedriver.exe";
+	String CHROME_PATH = "./driver/chromedriver1.exe";
 	String FIREFOX_PATH = "./driver/geckodriver.exe";
 	String EDGE_PATH = "E:/Selenium/msedgedriver.exe";
 	String LOGIN_EXCEL = "./ExternalData/loginData.xlsx";
